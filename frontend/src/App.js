545f703356a35219aa453994b5107cff7b6156b3
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-08-11 00:43:42 +0200

    Signup / Signin, switch mode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,7 @@ import MainHeader from './components/MainHeader';
 import UserPlaces from './pages/UserPlaces';
 import NewPlace from './pages/NewPlace';
 import UpdatePlace from './pages/UpdatePlace';
+import Auth from './pages/Auth';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
           <Route path='/:userId/places' exact component={UserPlaces} />
           <Route path='/places/new' component={NewPlace} />
           <Route path='/places/:placeId' component={UpdatePlace} />
+          <Route path='/auth' exact component={Auth} />
           <Redirect to='/' />
         </Switch>
       </main>
---
+++ frontend/src/pages/Auth.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+
+import Input from '../components/Input';
+import useForm from '../hooks/useForm';
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../functions/validators';
+import Button from '../components/Button';
+import Card from '../components/Card';
+
+function Auth() {
+  const [isSignUp, setIsSignUp] = useState(false);
+
+  const [formState, inputChange, setFormData] = useForm({
+    email: {
+      value: '',
+      isValid: false
+    },
+    password: {
+      value: '',
+      isValid: false
+    }
+  }, false)
+
+  const authSubmitHandler = event => {
+    event.preventDefault();
+    console.log(formState)
+  }
+
+  const switchIsSignUp = () => {
+    if (!isSignUp) {
+      setFormData({
+        ...formState.inputs,
+        name: {
+          value: '',
+          isValid: false
+        }
+      }, false)
+    } else {
+      setFormData({
+        ...formState.inputs,
+        name: undefined
+      }, formState.inputs.email.isValid && formState.inputs.password.isValid)
+    }
+    setIsSignUp((prev) => !prev)
+  }
+
+  return (
+    <Card className='authentication'>
+      <h2>{isSignUp ? 'Register' : 'Login'} required</h2>
+      <form onSubmit={authSubmitHandler}>
+        {isSignUp && <Input id='name' element='input' type='text' label='Your name' validators={[VALIDATOR_REQUIRE()]} onInput={inputChange} />}
+        <Input id='email' element='input' type='email' label='E-mail' validators={[VALIDATOR_EMAIL()]} errorText='Please, enter a valid email.' onInput={inputChange} />
+        <Input id='password' element='input' type='password' label='Password' validators={[VALIDATOR_MINLENGTH(6)]} errorText='Please, enter a valid password (at least 5 characters).' onInput={inputChange} />
+        <Button submit disabled={!formState.isValid}>LOGIN</Button>
+      </form>
+      <Button onClick={switchIsSignUp} inverse>SWITCH TO{isSignUp ? ' SIGNIN' : ' SIGNUP'}</Button>
+    </Card>
+  )
+}
+
+export default Auth
