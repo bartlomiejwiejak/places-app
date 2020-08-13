34ce3acfea9304fdb7a7f8276f8511847c3f22ad
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-08-13 23:19:25 +0200

    Updating places

--- frontend/src/pages/UpdatePlace.js
+++ frontend/src/pages/UpdatePlace.js
@@ -1,43 +1,22 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState, useContext } from 'react'
+import { useParams, useHistory } from 'react-router-dom';
 
 import Input from '../components/Input';
 import Button from '../components/Button';
 import { VALIDATOR_REQUIRE, VALIDATOR_MINLENGTH } from '../functions/validators';
 import useForm from '../hooks/useForm';
-import Card from '../components/Card';
-
-const PLACES = [
-  {
-    id: 'p1',
-    title: 'Warsaw',
-    description: 'Best city in eu',
-    imageUrl: 'https://s27363.pcdn.co/wp-content/uploads/2020/05/Things-to-do-in-Warsaw.jpg.optimal.jpg',
-    address: 'Sienna 39, 00-121 Warszawa',
-    location: {
-      lat: 52.2318392,
-      lng: 20.9998857
-    },
-    creator: 'u1'
-  },
-  {
-    id: 'p2',
-    title: 'Krakow',
-    description: 'Worst',
-    imageUrl: 'https://s27363.pcdn.co/wp-content/uploads/2020/05/Things-to-do-in-Warsaw.jpg.optimal.jpg',
-    address: 'Sienna 39, 00-121 Warszawa',
-    location: {
-      lat: 52.2318392,
-      lng: 20.9998857
-    },
-    creator: 'u2'
-  }
-]
+import LoadingSpinner from '../components/LoadingSpinner';
+import ErrorModal from '../components/ErrorModal';
+import useHttp from '../hooks/useHttp';
+import { AuthContext } from '../context/auth-context';
 
 function UpdatePlace() {
 
   const placeId = useParams().placeId
-  const [isLoading, setIsLoading] = useState(true);
+  const { sendRequest, isLoading, error, clearError } = useHttp();
+  const [loadedPlace, setLoadedPlace] = useState(null)
+  const history = useHistory()
+  const { userId } = useContext(AuthContext);
 
   const [formState, inputChange, setFormData] = useForm({
     title: {
@@ -50,51 +29,62 @@ function UpdatePlace() {
     },
   }, false)
 
-  const identifiedPlace = PLACES.find(place => place.id === placeId)
   useEffect(() => {
-    if (identifiedPlace) {
-      setFormData({
-        title: {
-          value: identifiedPlace.title,
-          isValid: true
-        },
-        description: {
-          value: identifiedPlace.description,
-          isValid: true
-        },
-      }, true)
+    const fetchPlace = async () => {
+      try {
+        const responseData = await sendRequest(`http://localhost:5000/api/places/${placeId}`)
+        const identifiedPlace = responseData.place;
+        setLoadedPlace(identifiedPlace);
+        setFormData({
+          title: {
+            value: identifiedPlace.title,
+            isValid: true
+          },
+          description: {
+            value: identifiedPlace.description,
+            isValid: true
+          },
+        }, true)
+      } catch (err) { }
     }
-    setIsLoading(false)
-  }, [setFormData, identifiedPlace])
+    fetchPlace();
+  }, [placeId, sendRequest, setFormData])
 
-  if (!identifiedPlace) {
+  if (isLoading) {
     return (
-      <div className="center">
-        <Card>
-          <h2>Could not find place!</h2>
-        </Card>
-      </div>
+      <LoadingSpinner asOverlay />
     )
+  }
+
+  if (!loadedPlace && !error) {
+    return <LoadingSpinner asOverlay />
   }
 
   const placeUpdateSubmitHandler = event => {
     event.preventDefault();
-  }
-
-  if (isLoading) {
-    return (
-      <div>
-        <h2>Loading...</h2>
-      </div>
-    )
+    const updatePlace = async () => {
+      try {
+        await sendRequest(`http://localhost:5000/api/places/${placeId}`, 'PATCH', JSON.stringify({
+          title: formState.inputs.title.value,
+          description: formState.inputs.description.value,
+        }), {
+          'Content-Type': 'application/json'
+        })
+        history.push('/' + userId + '/places');
+      } catch (err) { }
+    }
+    updatePlace();
   }
 
   return (
-    <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
-      <Input id='title' element='input' type='text' label='Title' validators={[VALIDATOR_REQUIRE()]} errorText='Please, enter a valid title.' initialValue={formState.inputs.title.value} valid={formState.inputs.title.isValid} onInput={inputChange} />
-      <Input id='title' element='textarea' label='Description' validators={[VALIDATOR_MINLENGTH(5)]} errorText='Please, enter a valid description (at least 5 characters).' initialValue={formState.inputs.description.value} valid={formState.inputs.description.isValid} onInput={inputChange} />
-      <Button type='submit' disabled={!formState.isValid}>UPDATE PLACE</Button>
-    </form>
+    <>
+      <ErrorModal onClear={clearError} error={error} />
+      {!isLoading && loadedPlace && <form className='place-form' onSubmit={placeUpdateSubmitHandler}>
+        <Input id='title' element='input' type='text' label='Title' validators={[VALIDATOR_REQUIRE()]} errorText='Please, enter a valid title.' initialValue={loadedPlace.title} valid={true} onInput={inputChange} />
+        <Input id='description' element='textarea' label='Description' validators={[VALIDATOR_MINLENGTH(5)]} errorText='Please, enter a valid description (at least 5 characters).' initialValue={loadedPlace.description} valid={true} onInput={inputChange} />
+        <Button type='submit' disabled={!formState.isValid}>UPDATE PLACE</Button>
+      </form>}
+    </>
   )
 }
 
