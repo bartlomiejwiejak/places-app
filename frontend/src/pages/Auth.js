ccbfcdfb552236a03fe677b3dc00b7e1e939f5bb
Wiejak Bartłomiej <bartlomiej.wiejak98@gmail.com>
2020-08-13 16:50:01 +0200

    Signup request and loading / error state

--- frontend/src/pages/Auth.js
+++ frontend/src/pages/Auth.js
@@ -6,10 +6,14 @@ import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH, VALIDATOR_REQUIRE } from '../func
 import Button from '../components/Button';
 import Card from '../components/Card';
 import { AuthContext } from '../context/auth-context';
+import LoadingSpinner from '../components/LoadingSpinner';
+import ErrorModal from '../components/ErrorModal';
 
 function Auth() {
   const [isSignUp, setIsSignUp] = useState(false);
   const { login } = useContext(AuthContext);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const [formState, inputChange, setFormData] = useForm({
     email: {
@@ -27,6 +31,7 @@ function Auth() {
     let response;
     if (isSignUp) {
       try {
+        setIsLoading(true);
         response = await fetch('http://localhost:5000/api/users/signup', {
           method: 'POST',
           headers: {
@@ -39,10 +44,15 @@ function Auth() {
           })
         })
         const responseData = await response.json()
-        console.log(responseData)
+        if (!response.ok) {
+          throw new Error(responseData.message)
+        }
+        setIsLoading(false);
+        login();
       } catch (err) {
+        setError(err.message)
+        setIsLoading(false);
       }
-      login();
     }
   }
 
@@ -64,17 +74,25 @@ function Auth() {
     setIsSignUp((prev) => !prev)
   }
 
+  const errorHandler = () => {
+    setError(null)
+  }
+
   return (
-    <Card className='authentication'>
-      <h2>{isSignUp ? 'Register' : 'Login'} required</h2>
-      <form onSubmit={authSubmitHandler}>
-        {isSignUp && <Input id='name' element='input' type='text' label='Your name' validators={[VALIDATOR_REQUIRE()]} onInput={inputChange} />}
-        <Input id='email' element='input' type='email' label='E-mail' validators={[VALIDATOR_EMAIL()]} errorText='Please, enter a valid email.' onInput={inputChange} />
-        <Input id='password' element='input' type='password' label='Password' validators={[VALIDATOR_MINLENGTH(6)]} errorText='Please, enter a valid password (at least 5 characters).' onInput={inputChange} />
-        <Button submit disabled={!formState.isValid}>{isSignUp ? 'SIGNUP' : 'LOGIN'}</Button>
-      </form>
-      <Button onClick={switchIsSignUp} inverse>SWITCH TO{isSignUp ? ' SIGNIN' : ' SIGNUP'}</Button>
-    </Card>
+    <>
+      {error ? <ErrorModal onClear={errorHandler} error={error} /> : null}
+      <Card className='authentication'>
+        {isLoading ? <LoadingSpinner asOverlay /> : null}
+        <h2>{isSignUp ? 'Register' : 'Login'} required</h2>
+        <form onSubmit={authSubmitHandler}>
+          {isSignUp && <Input id='name' element='input' type='text' label='Your name' validators={[VALIDATOR_REQUIRE()]} onInput={inputChange} />}
+          <Input id='email' element='input' type='email' label='E-mail' validators={[VALIDATOR_EMAIL()]} errorText='Please, enter a valid email.' onInput={inputChange} />
+          <Input id='password' element='input' type='password' label='Password' validators={[VALIDATOR_MINLENGTH(6)]} errorText='Please, enter a valid password (at least 5 characters).' onInput={inputChange} />
+          <Button submit disabled={!formState.isValid}>{isSignUp ? 'SIGNUP' : 'LOGIN'}</Button>
+        </form>
+        <Button onClick={switchIsSignUp} inverse>SWITCH TO{isSignUp ? ' SIGNIN' : ' SIGNUP'}</Button>
+      </Card>
+    </>
   )
 }
 
